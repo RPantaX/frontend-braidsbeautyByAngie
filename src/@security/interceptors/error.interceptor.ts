67bc3d7bbf9b60df6fc845c7fcc513af63f6de88
Jefferson Panta <pantajefferson173@gmail.com>
2025-05-10 22:42:40 -0500

    feat(Interceptor): add interceptor 500 error

---
+++ src/@security/interceptors/error.interceptor.ts
@@ -0,0 +1,40 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { catchError, Observable, throwError } from "rxjs";
+import { APIErrorResponse } from "../../@utils/interfaces/ApiResponse";
+
+import { inject, Injectable } from '@angular/core';
+import { MessageService } from "../../app/shared/message/message.service";
+import { TranslateService } from "@ngx-translate/core";
+
+@Injectable({providedIn: 'root'})
+export class SecurityInterceptor implements HttpInterceptor{
+  private readonly _messageService: MessageService = inject(MessageService);
+  private readonly _translate = inject(TranslateService);
+  private readonly _snackBar = this._messageService.snackBar;
+	private readonly _duration = 5000;
+  constructor() { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const apiResponse = error.error as APIErrorResponse;
+        const internalServerErrorMessage =  this._translate.instant('errors.serverError');
+        const { message, status  } = apiResponse;
+       if (status === 500 || status === 503) {
+        		this._snackBar.error(message || internalServerErrorMessage, this._duration);
+						return throwError(() => new Error(status.toString()));
+       } else if (status === 403) {
+         // Maneja el error de autorización aquí
+         console.error('Error de autorización:', error);
+       } else {
+         // Maneja otros errores aquí
+         console.error('Error:', error);
+       }
+       return throwError(() => error);
+     })
+    );
+
+    throw new Error("Method not implemented.");
+  }
+
+}
---
+++ src/@utils/interfaces/ApiResponse.ts
@@ -0,0 +1,7 @@
+export interface APIErrorResponse {
+  status:    number;
+  timestamp: Date;
+  message:   string;
+  details:   null;
+  path:      string;
+}
